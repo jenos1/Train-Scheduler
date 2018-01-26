86cd824b1e8737145bd64fc4098d5cd042e76627
bterrier <joann_enos@verizon.net>
2018-01-26 08:23:07 -0500

    Updates for train table.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,64 @@
 $(document).ready(function() {
 
 	// Initialize Firebase
-	var config = {
-	  apiKey: "AIzaSyA2_y_RdRtwMuAg33LHlBCZ0U8JqRwJY-0",
-	  authDomain: "fir-click-counter-7cdb9.firebaseapp.com",
-	  databaseURL: "https://fir-click-counter-7cdb9.firebaseio.com",
-	  storageBucket: "fir-click-counter-7cdb9.appspot.com"
-	  };
+		var config = {
+			apiKey: "AIzaSyAhzbkgzUqUZloR9SCVe3LtboKh20u0aaw",
+			authDomain: "trains-f0aaf.firebaseapp.com",
+			databaseURL: "https://trains-f0aaf.firebaseio.com",
+			projectId: "trains-f0aaf",
+			storageBucket: "",
+			messagingSenderId: "38292520638"
+		};
 
 	firebase.initializeApp(config);
 
-	// Get a reference to the database service
+	// Get a reference to the database service & assign variables.
   var database = firebase.database();
+  var name = "";
+  var dest = ""; 
+  var freq = 0;
+  var nextArrival = 0;
+  var minsAway = 0;
+	var time = 0;
 
+$("#submit").on("click", function(event) {
+	event.preventDefault();
+
+	name = $("#name-input").val().trim();
+	dest = $("#dest-input").val().trim();
+	time = $("#time-input").val().trim();
+	freq = $("#freq-input").val().trim();	
+
+	console.log("Name:", name);
+	console.log("dest:", dest);
+	console.log("time:", time);
+	console.log("freq:", freq);
+
+	var trainInput = {
+		name: name,
+		dest: dest,
+		time: time,
+		freq: freq
+	}
+	database.ref("trains").push(trainInput);
+})
+
+	database.ref("trains").on("child_added", function(trainData) {
+		console.log(trainData.val());
+		// $("#train-table tr").remove(); 
+		// $("#list tbody").remove();
+		$("#train-list").append(
+		// "<tr><td>" + name + dest + time + freq + nextArrival + minsAway + "</td></tr>");
+		"<tr><td>" + name +
+		"<tr><td>" + dest +	
+		"<tr><td>" + time +	
+		"<tr><td>" + freq +
+		"<tr><td>" + nextArrival +
+		"<tr><td>" + minsAway +
+		"<td><tr>");
+
+	})
 
 
 })
+
