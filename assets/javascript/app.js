c418b7442a54a81179ea74a2daea57c424406431
bterrier <joann_enos@verizon.net>
2018-01-28 20:02:40 -0500

    Add NextArrival & MinutesAway.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,56 +10,94 @@ $(document).ready(function() {
 			messagingSenderId: "38292520638"
 		};
 
-	firebase.initializeApp(config);
+		firebase.initializeApp(config);
 
-	// Get a reference to the database service & assign variables.
-  var database = firebase.database();
-  var name = "";
-  var dest = ""; 
-  var freq = 0;
-  var nextArrival = 0;
-  var minsAway = 0;
-	var time = 0;
+		var database = firebase.database();
 
-$("#submit").on("click", function(event) {
-	event.preventDefault();
+		// Assign variables. 
+	  var name = "";
+	  var dest = ""; 
+	  var freq = 0;
+	  var nextArrival = 0;
+	  var minsAway = 0;
+	  var time = 0;
 
-	name = $("#name-input").val().trim();
-	dest = $("#dest-input").val().trim();
-	time = $("#time-input").val().trim();
-	freq = $("#freq-input").val().trim();	
+		$("#submit").on("click", function(event) {
+			// Prevent page refresh to preserve data.
+			event.preventDefault();
 
-	console.log("Name:", name);
-	console.log("dest:", dest);
-	console.log("time:", time);
-	console.log("freq:", freq);
+			// Stores user input in existing variables.
+			name = $("#name-input").val().trim();
+			dest = $("#dest-input").val().trim();
+			time = $("#time-input").val().trim();
+			freq = $("#freq-input").val().trim();	
 
+			console.log("Name:", name);
+			console.log("dest:", dest);
+			console.log("time:", time);
+			console.log("freq:", freq);
 
-	var trainInput = {
-		name: name,
-		dest: dest,
-		time: time,
-		freq: freq,
-	}
-	database.ref("trains").push(trainInput);
-})
+			// Creates an object to hold train data
+			var trainInput = {
+				name: name,
+				dest: dest,
+				time: time,
+				freq: freq,
+			}
 
-	database.ref("trains").on("child_added", function(trainData) {
-		console.log(trainData.val());
-		// $("#train-table tr").remove(); 
-		// $("#list tbody").remove();
-		$("#train-list").append(
-		// "<tr><td>" + name + dest + time + freq + nextArrival + minsAway + "</td></tr>");
-		"<tr><td>" + name +
-		"<tr><td>" + dest +	
-		"<tr><td>" + time +	
-		"<tr><td>" + freq +
-		"<tr><td>" + nextArrival +
-		"<tr><td>" + minsAway +
-		"<td><tr>");
+			// Moment.js time calculations for trains.
+			// Current Time
+			var currTime = moment().format("HH:mm");
+			console.log("Current Time is: ", currTime);
+
+			// First Time converstion (Must be pushed back 1 year to make sure it comes before current time).
+			var firstTimeConverted = moment(time, "HH:mm").subtract(1, "years");
+			console.log("FirstTimeConverted: ", firstTimeConverted);
+
+			// Difference between current time & converted time in minutes.
+			var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+			console.log("TimeDiff now & firstArrivaltime converted: ", diffTime);
+
+			// Time apart (remainder)
+		  var tRemainder = diffTime % freq;
+		  console.log("Remainder: ", tRemainder);
+
+		  // Minutes until next train.
+		  var minsAway = freq - tRemainder;
+		  console.log("Minutes Away: ", minsAway);
+
+		  var nextTrain = (moment().add(minsAway, "minutes")).format("hh:mm");
+		  var nextArrival = nextTrain.toString();
+	    console.log(nextArrival);
+
+			// Loads data into Firebase db.
+			database.ref().push(trainInput);
+			// $(".form-control").val('');
+
+			// Create Firebase listener for child changes then download data to client table.
+			database.ref("trains").on("child_added", function(trainData) {
+			console.log(trainData.val());
+
+			$("#name-input").val("");
+			$("#dest-input").val("");
+			$("#time-input").val("");
+			$("#freq-input").val("");
+	  });
+
+				$("#train-list").append(
+		"<tr><td>" + name + "<td>" + dest  + "<td>" + freq + "<td>" + nextArrival + "<td>" + minsAway + "</td></tr>");
+
+		//can create childnode using /users: database.ref("/users").push
+		//your node is your key - key is node of the snapshot. 
+		//console.log(snapshot.key) - how we update data later. /users/keyvalue
+		// says give me back the timestamp that your server says -- in its own time zone.
+		//moment js can then format it correct. It is in diff format.
+		//child_added fires when there is one added. See diff of value vs. child_added in the documentation.
+		//ref() is root of db. orderbyChild - orders by dateadded and limits to one record - could be last if want.
+		//so it sorts and filters. 
+		//snapshot gives back full value (ref) from root & child only the child value.
+		//use Unix to store your data then convert back as needed
 
 	})
-
-
 })
 
